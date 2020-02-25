667699e9421c797cc112f64b969b8db93450650f
unknown <svclennan@icloud.com>
2020-02-25 15:57:13 -0600

    table shows the players still in the game and the previous round score. winners table appears on a bottom table which is added to for each round.

--- dice.js
+++ dice.js
@@ -2,57 +2,69 @@
 
 var playerOne = {
 	name: "Sean",
-	roundScore: 0
+	roundScore: 0,
+	wins: 0
 }
 var playerTwo = {
 	name: "Zac",
-	roundScore: 0
+	roundScore: 0,
+	wins: 0
 }
 var playerThree = {
 	name: "Austin",
-	roundScore: 0
+	roundScore: 0,
+	wins: 0
 }
 var playerFour = {
 	name: "Matt",
-	roundScore: 0
+	roundScore: 0,
+	wins: 0
 }
 var playerFive = {
 	name: "Gustavo",
-	roundScore: 0
+	roundScore: 0,
+	wins: 0
 }
 var playerSix = {
 	name: "Nevin",
-	roundScore: 0
+	roundScore: 0,
+	wins: 0
 }
 var playerSeven = {
 	name: "Mike T",
-	roundScore: 0
+	roundScore: 0,
+	wins: 0
 }
 var playerEight = {
 	name: "Mike H",
-	roundScore: 0
+	roundScore: 0,
+	wins: 0
 }
 var playerNine = {
 	name: "David",
-	roundScore: 0
+	roundScore: 0,
+	wins: 0
 }
 var playerTen = {
 	name: "Brett",
-	roundScore: 0
+	roundScore: 0,
+	wins: 0
 }
 
 var playersAlive = [
-	playerOne,
-	playerTwo,
-	playerThree,
-	playerFour,
-	playerFive,
-	playerSix,
-	playerSeven,
-	playerEight,
-	playerNine,
-	playerTen
+playerOne,
+playerTwo,
+playerThree,
+playerFour,
+playerFive,
+playerSix,
+playerSeven,
+playerEight,
+playerNine,
+playerTen
 ]
+
+var gameWinners = []
 
 function rollD4(){
 	return (Math.floor(Math.random()*4)+1);
@@ -87,6 +99,7 @@ function finalDice(){
 		case(4): return d4;
 	}
 }
+var roundNumber = 1;
 
 function runRound(){
 	for(let i = 0; i < playersAlive.length; i++){
@@ -115,15 +128,74 @@ function finalRound(){
 	alert(lastPlayer.name + " has lost with a score of " + lastPlayer.roundScore + ".");
 	lastPlayer = playersAlive.pop();
 	alert(lastPlayer.name + " wins the game with a score of " + lastPlayer.roundScore + "!!!");
+	document.getElementById("winner").innerHTML = "<tr class = table-info><td class = text-center>" + lastPlayer.name + " won!!!</tr></td>";
+	var x = document.getElementById("snackbar")
+	x.innerHTML = lastPlayer.name + " wins!!!";
+	x.className = "show";
+	setTimeout(function(){ x.className = x.className.replace("show", ""); }, 2500);
+	lastPlayer.wins += 1;
+	if(!(gameWinners.includes(lastPlayer))){
+		gameWinners.push(lastPlayer);
+	}
 }
 
 function runGame(){
-	for(let i = 0; i < 3; i++){
+	document.getElementById("progressButton").innerHTML = "Next Round";
+	if(roundNumber <= 3){
 		runRound();
+		roundNumber++;
+		makeTable();
+
 	}
-	for(let i = 0;i<2;i++){
+	else if(roundNumber < 6){
 		runFinalRounds();
+		roundNumber++;
+		makeTable();
 	}
-	alert("Dice Shootout!!");
-	finalRound();
+	else{
+		alert("Dice Shootout!!");
+		finalRound();
+		playersAlive = [
+		playerOne,
+		playerTwo,
+		playerThree,
+		playerFour,
+		playerFive,
+		playerSix,
+		playerSeven,
+		playerEight,
+		playerNine,
+		playerTen
+		]
+		roundNumber = 1;
+		document.getElementById("progressButton").innerHTML = "Play again?";
+		makeTable();
+		showWinner();
+	}
 }
+
+function makeTable(){
+	var t = '<tr class = "table-danger" ><td style= "width: 75%";>Round '+ roundNumber + '</td><td style = "width: 25%";>Round Score</td></tr>';
+	for(var i = 0; i < playersAlive.length; i++){
+		var tr = '<tr class = "table-success border">';
+		tr+= '<td class = "text-center border">'+playersAlive[i].name + '</td>';
+		tr+= '<td class = "text-center border">'+playersAlive[i].roundScore + '</td>';
+		tr+="</tr>";
+		t += tr
+	}
+	document.getElementById("table").innerHTML = t;
+}
+makeTable();
+
+function showWinner(){
+	var t = "<tr class = table-warning><th>Winners</th><th>Wins</th></tr>";
+	for(var i = 0; i < gameWinners.length; i++){
+		var tr = "<tr class = table-warning text-center>";
+		tr+= "<td class = text-center>"+gameWinners[i].name + "</td>";
+		tr+= "<td class = text-center>"+gameWinners[i].wins + "</td>";
+		tr+="</tr>";
+		t += tr
+	}
+	document.getElementById("prevWinner").innerHTML = t;
+}
+makeTable();
